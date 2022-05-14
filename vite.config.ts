0d5870ae77ba0fd270a252188078ebc851dc4e51
Robbie <robbie@reformcollective.com>
2022-05-14 14:48:35 -0600

    add some pwa stuff

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,30 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["assets/favicon.ico", "assets/apple-touch-icon.png"],
+      manifest: {
+        name: "Cartainment",
+        short_name: "car",
+        description: "car stuff for myself",
+        theme_color: "purple",
+        icons: [
+          {
+            src: "/assets/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/assets/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 3000,
     https: true,
