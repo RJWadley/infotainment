42e362efe0a5f464258326f4a0a35c1bca01e43a
Robbie <robbie@reformcollective.com>
2022-05-14 15:33:04 -0600

    first try

--- public/sw.js
+++ public/sw.js
@@ -3,26 +3,26 @@
 //on install cache resources
 self.addEventListener("install", function (e) {
   e.waitUntil(
-    caches.open("cache-v1").then(function (cache) {
+    caches.open("cache").then(function (cache) {
       //add everything to cache
       return cache.addAll(["/index.html"]);
     })
   );
 });
 
+//on fetch, request from network
+//if network success, add to cache
+//if network fail, return from cache
 self.addEventListener("fetch", function (e) {
   e.respondWith(
     fetch(e.request)
-      .then(
-        //add to cache
-        function (response) {
-          return caches.open("cache-v1").then(function (cache) {
-            cache.put(e.request, response.clone());
-            return response;
-          });
-        }
-      )
-      .catch(function () {
+      .then(function (res) {
+        return caches.open("cache").then(function (cache) {
+          cache.put(e.request, res.clone());
+          return res;
+        });
+      })
+      .catch(function (err) {
         return caches.match(e.request);
       })
   );
